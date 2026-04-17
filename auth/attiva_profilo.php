a564d3080d392f168c0cefd6b064f796ebef1722
Pada07 <sm.padalino@mail.scuole.vda.it>
2026-04-17 11:58:51 +0200

    Delete auth directory

--- auth/attiva_profilo.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['id_account_attivo'] = $_GET['id_acc']; // prendo il valore dell'ID Account usando la GET. il valore mi è stato passato utilizzando la sintassi "?id_acc=IDACCOUT"
-$_SESSION['nome_account_attivo'] = $_GET['nome_acc'];
-
-// Usciamo da auth/ ed entriamo in catalogo/
-header("Location: ../catalogo/home.php");
-exit();
-?>
--- auth/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // elimina TUTTE le variabili denro $_SESSION[]
-header("Location: auth.php");
-exit();
-?>
